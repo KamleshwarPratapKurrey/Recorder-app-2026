6a57937cec3cf3f54fba6d8236d433125a20151b
Kamleshwar Pratap Kurrey <kamleshwarpratapkurrey@gmail.com>
2026-06-19 13:03:16 +0530

    upd

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -7,7 +7,6 @@ import {
   Alert,
   Animated,
   FlatList,
-  SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
@@ -15,6 +14,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface RecordingItem {
   id: string;
